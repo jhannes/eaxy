115eeb8852089318a7748d8405f3259172feeb29
unknown <jolous@NOLJOLOUS01.one.steria.dom>
2014-10-01 12:56:46 +0200

    Tighteneded up testing

--- src/test/java/org/eaxy/usage/FileDecodingTest.java
+++ src/test/java/org/eaxy/usage/FileDecodingTest.java
@@ -9,17 +9,15 @@ import org.junit.Test;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class FileDecodingTest {
 
-    public static final String BAD = "ISO-8859-1";
     public static final String GOOD = "UTF-8";
     private static String oldSystemDefaultEncoding;
 
@@ -34,55 +32,23 @@ public class FileDecodingTest {
     }
 
     @Test
-    public void shouldReproducePlainText() throws IOException, URISyntaxException {
-        File file = new File(getClass().getResource("/testdocument.html").toURI());
-
-        Document document = Xml.read(file);
-
-        assertThat(document.toXML()).contains("Hello world");
-    }
-
-    @Test
-    public void shouldReadUtf8ByDefault() throws IOException, URISyntaxException, NoSuchFieldException, IllegalAccessException {
-        setSystemDefaultEncoding(GOOD);
-        File file = new File(getClass().getResource("/testdocument.html").toURI());
-
-        FileReader reader = new FileReader(file);
-        String hit = new Scanner(reader).findWithinHorizon(Pattern.compile("some n.+n letters"), 1000);
-        reader.close();
-
-        assertThat(hit).contains("nørwægiån");
-    }
-
-    @Test
-    public void shouldFailAtUTF8ByDefault() throws IOException, URISyntaxException, NoSuchFieldException, IllegalAccessException {
-        setSystemDefaultEncoding(BAD);
-        File file = new File(getClass().getResource("/testdocument.html").toURI());
-
-        FileReader reader = new FileReader(file);
-        String hit = new Scanner(reader).findWithinHorizon(Pattern.compile("some n.+n letters"), 1000);
-        reader.close();
-
-        assertThat(hit).contains("nÃ¸rwÃ¦giÃ¥n");
-    }
-
-    @Test
     public void shouldReadUtf8DespiteDefault() throws IOException, URISyntaxException, NoSuchFieldException, IllegalAccessException {
-        setSystemDefaultEncoding(BAD);
+        setSystemDefaultEncoding("ISO-8859-1");
         File file = new File(getClass().getResource("/testdocument.html").toURI());
 
+        String reference = slurp(file);
         Document document = Xml.read(file);
 
+        assertThat(reference).contains("nÃ¸rwÃ¦giÃ¥n");
         assertThat(document.toXML()).contains("nørwægiån");
     }
 
-
     private static void setSystemDefaultEncoding(String encoding) {
         System.setProperty("file.encoding", encoding);
-        invalidateCachedFieldValue(Charset.class, "defaultCharset");
+        invalidateFieldCache(Charset.class, "defaultCharset");
     }
 
-    private static void invalidateCachedFieldValue(Class classObject, String fieldName) {
+    private static void invalidateFieldCache(Class classObject, String fieldName) {
         try {
             Field field = classObject.getDeclaredField(fieldName);
             field.setAccessible(true);
@@ -92,6 +58,13 @@ public class FileDecodingTest {
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    private String slurp(File file) throws FileNotFoundException {
+        Scanner scan = new Scanner(file);
+        scan.useDelimiter("\\Z");
+        return scan.next();
     }
 
 }
